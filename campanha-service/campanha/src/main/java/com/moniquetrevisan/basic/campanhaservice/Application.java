880bf36112a530c80f0a7b21513ad54a70cc1dc2
Monique Trevisan <moniquetrevisan@hotmail.com>
2018-04-28 20:26:22 -0300

    revisando as configs do client/serve eureka para testar

--- campanha-service/campanha/src/main/java/com/moniquetrevisan/basic/campanhaservice/Application.java
+++ campanha-service/campanha/src/main/java/com/moniquetrevisan/basic/campanhaservice/Application.java
@@ -1,5 +1,6 @@
 package com.moniquetrevisan.basic.campanhaservice;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		SpringApplication.run(Application.class, args);
 	}
 
 }
